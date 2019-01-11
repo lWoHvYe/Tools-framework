dbdd8ac7c1df113364fc7ebbe19bb5f45b5701a8
lWoHvYe <39266195+lWoHvYe@users.noreply.github.com>
2019-01-11 16:24:52 +0800

    Tomcat-Session共享

---
+++ tomcat-redis-session-manager-8.5/README.md
@@ -0,0 +1,5 @@
+环境支持
+Tomcat 8.5.35
+redis 3.0
+
+redis地址:192.168.120.110 端口号:6379 密码：redis   这三项在RedisSessionManager中修改 
---
+++ tomcat-redis-session-manager-8.5/src/main/java/com/orangefunction/tomcat/redissessions/LifecycleSupport.java
@@ -0,0 +1,40 @@
+package com.orangefunction.tomcat.redissessions;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import org.apache.catalina.Lifecycle;
+import org.apache.catalina.LifecycleEvent;
+import org.apache.catalina.LifecycleListener;
+
+public final class LifecycleSupport {
+    private final Lifecycle lifecycle;
+    private final List<LifecycleListener> listeners = new CopyOnWriteArrayList();
+
+    public LifecycleSupport(Lifecycle lifecycle) {
+        this.lifecycle = lifecycle;
+    }
+
+    public void addLifecycleListener(LifecycleListener listener) {
+        this.listeners.add(listener);
+    }
+
+    public LifecycleListener[] findLifecycleListeners() {
+        return (LifecycleListener[])this.listeners.toArray(new LifecycleListener[0]);
+    }
+
+    public void fireLifecycleEvent(String type, Object data) {
+        LifecycleEvent event = new LifecycleEvent(this.lifecycle, type, data);
+        Iterator i$ = this.listeners.iterator();
+
+        while(i$.hasNext()) {
+            LifecycleListener listener = (LifecycleListener)i$.next();
+            listener.lifecycleEvent(event);
+        }
+
+    }
+
+    public void removeLifecycleListener(LifecycleListener listener) {
+        this.listeners.remove(listener);
+    }
+}
